8683c809b3a2b2a9b1f972fccfd18f0292c903bf
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-05-13 17:59:27 +0500

    Update README.md
    doc: add list of entityes

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module TestGuru
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.time_zone = "Ekaterinburg"
+
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
+    config.i18n.default_locale = :ru
   end
 end
