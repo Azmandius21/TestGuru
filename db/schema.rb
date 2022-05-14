985afc80478f2e1d77087200d452f7d1c930378b
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-05-14 16:35:36 +0500

    feat: created models models: user, question, category, answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_14_093338) do
+ActiveRecord::Schema.define(version: 2022_05_14_101405) do
 
   create_table "tests", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.integer "level"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
