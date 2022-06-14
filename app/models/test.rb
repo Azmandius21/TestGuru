930f4baf3c51fc09d96e5e90d6f399d002848593
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-06-14 22:20:51 +0500

    fix: models/test.rb: add return array.sort.reverse!

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,8 @@
 class Test < ApplicationRecord
 
-  def self.sort_desc_from_category(category)
+  def self.sort_desc_current_category(category)
     #self.joins(:category).where(category_id: Category.find_by(title: category).id).order(title: :desc).pluck(:title)
     self.joins("JOIN categories ON tests.category_id = categories.id
-      WHERE categories.title = '#{category}' ORDER BY title DESC")
+      WHERE categories.title = '#{category}'").pluck(:title).sort.reverse!
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
 
   def show_passed_tests(level=1)
-    Test.joins("JOIN questions ON tests.id = questions.test_id JOIN answers ON questions.id = answers.question_id
-      WHERE answers.author_id = #{self.id} AND  tests.level = #{level}")
+    Test.joins("JOIN questions
+                ON tests.id = questions.test_id
+                JOIN answers
+                ON questions.id = answers.question_id
+                WHERE answers.author_id = #{self.id}
+                AND  tests.level = #{level}")
   end
 end
