c179b6855ad436d86fb40273c610f059041f8a9d
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-06-18 10:03:55 +0500

    fix: association test with users through tests_users

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,7 +4,7 @@ class Test < ApplicationRecord
 
   has_many :questions, dependent: :destroy
   has_many :tests_users, dependent: :destroy
-  has_many :users, through: :test_users
+  has_many :users, through: :tests_users
 
   def self.sort_desc_current_category(title_category)
     joins('JOIN categories ON tests.category_id = categories.id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-
-  has_many :created_tests, class_name: 'Test', dependent: :destroy
+  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
   has_many :tests_users, dependent: :destroy
   has_many :tests, through: :tests_users
 
