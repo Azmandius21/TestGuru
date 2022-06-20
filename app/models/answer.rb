eecba910d2bddd385acc40d7c2729455491e61f3
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-06-20 12:29:08 +0500

    feat: validation on answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,4 +4,12 @@ class Answer < ApplicationRecord
   scope :right, ->{where(correct: true)}
 
   validates :body, presence: true
+  validate :limit_answer_on_question
+
+  private
+
+  def limit_answer_on_question
+    num_answ_on_one_quest = Answer.where(question_id: self.question_id).count
+    errors.add(:id) unless [1, 2, 3, 4].include?(num_answ_on_one_quest)
+  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -10,10 +10,9 @@ class Test < ApplicationRecord
   scope :medium, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
   scope :sort_current_category, -> (title_category){ joins(:category).where(categories: {title: title_category}).order(title: :DESC).pluck(:title) }
-  scope :title_level, -> { select(:title, :level)}
 
-  validates :title, presence: true
-  validates :title, uniqueness: { scope: [:title, :level]}
+  validates :title, presence: true,
+                    uniqueness: { scope: [:title, :level]}
   validates :level, numericality: { only_integer: true }
 
   def self.sort_desc_current_category(title_category)
