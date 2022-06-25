530f04058acd7b5bbc804000c43bb7e413fc851f
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-06-25 23:10:49 +0500

    fix(model): add sort DESC for scope :sort_current_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -10,13 +10,13 @@ class Test < ApplicationRecord
   scope :medium, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
   scope :sort_current_category,
-    lambda { |title| joins(:category).where(categories: { title: title })}
+    lambda { |title| joins(:category).where(categories: { title: title }).order(title: :DESC)}
 
   validates :title, presence: true,
                     uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true }
 
   def self.sort_desc_current_category(title_category)
-    sort_current_category(title_category).order(title: :DESC).pluck(:title)
+    sort_current_category(title_category).pluck(:title)
   end
 end
