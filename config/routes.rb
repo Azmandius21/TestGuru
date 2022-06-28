e465328424a35528791312f3ce6a79d94c78e290
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-06-28 10:57:56 +0500

    feat: routs.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #resources :tests
+
+  #resource :account, controller: :account
+
+  # resources :tests, only: %i[index show]
+
+  # resources :tests do
+  #   resources :questions
+  # end
+
+  resources :tests do
+    resources :questions, shallow: true
+  end
 end
