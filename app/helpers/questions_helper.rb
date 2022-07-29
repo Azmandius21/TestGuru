bd33fafeef3191532ec3a511fea3fca534b478b7
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-07-29 19:20:37 +0500

    feat: add helpers for Questions

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,17 @@
 module QuestionsHelper
+  def question_header(test, question)
+    if question.new_record?
+      "Create New Test #{test.title} Question"
+    else
+      "Edit Test #{test.title} Question"
+    end
+  end
+
+  def current_year
+    Time.now.year
+  end
+
+  def github_url(author,repo)
+    link_to "#{author}", "#{repo}", target: "_blank" 
+  end
 end
