affa4d2b45c1d193bff539debd35f49f7d9282c2
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-07-30 07:44:48 +0500

    fix: edit application_helper and questions_helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,17 +1,9 @@
 module QuestionsHelper
-  def question_header(test, question)
+  def question_header(question)
     if question.new_record?
-      "Create New Test #{test.title} Question"
+      "Create New Test #{question.test.title} Question"
     else
-      "Edit Test #{test.title} Question"
+      "Edit Test #{question.test.title} Question"
     end
-  end
-
-  def current_year
-    Time.now.year
-  end
-
-  def github_url(author,repo)
-    link_to "#{author}", "#{repo}", target: "_blank" 
   end
 end
