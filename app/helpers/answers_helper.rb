5af7c71edcf49bf2e6874716d6a0c24dc37b9d59
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-08-20 09:36:15 +0500

    fix:(must fix) answer view new and edit

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,2 +1,9 @@
 module AnswersHelper
+  def answer_header(answer)
+    if answer.new_record?
+      "Create new answer on question '#{answer.question.body}'"
+    else
+      "Edit new answer on question '#{answer.question.body}'"
+    end
+  end
 end
