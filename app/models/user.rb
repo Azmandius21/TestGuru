1a57b35638acd77fa43568dc8fe0886f291b1980
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-08-26 19:41:30 +0500

    feat: module Auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,14 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
+
+  include Auth
+
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
 
-  validates :email, presence: true
+  has_secure_password
 
   def show_passed_tests(level = 1)
     tests.where(level: level)
@@ -14,15 +17,5 @@ class User < ApplicationRecord
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
-  end
-
-  def authenticate(pasword_string)
-    digest.password_string == self.password_digest ? self : false
-  end
-
-  private
-
-  def digest(string)
-    Digest::SHA1.hexdigest(string)
   end
 end
