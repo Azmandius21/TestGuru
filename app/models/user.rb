ca15615cdd3620a9dc2582c89e86b5d9fc43aa92
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-08-26 17:30:07 +0500

    feat: create user_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'digest/sha1'
 
 class User < ApplicationRecord
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
@@ -13,5 +14,15 @@ class User < ApplicationRecord
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
+  end
+
+  def authenticate(pasword_string)
+    digest.password_string == self.password_digest ? self : false
+  end
+
+  private
+
+  def digest(string)
+    Digest::SHA1.hexdigest(string)
   end
 end
