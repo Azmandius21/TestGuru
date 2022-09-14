22d0786d718d67a33dc367c3c61191f11f647f1b
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-09-14 17:38:24 +0500

    fix: changed flash and applicationn_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parametrs, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parametrs
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:username, :email)
+    end
+  end
 end
