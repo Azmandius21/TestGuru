02d8f611f21ee9f70c8154e9fc73c65f64fbc29a
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-09-16 23:40:45 +0500

    fix: question controller add rescue

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,10 @@
 
 class QuestionsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  private
+
+  def rescue_with_question_not_found
+    render plain: 'Question was not found'
+  end
 end
