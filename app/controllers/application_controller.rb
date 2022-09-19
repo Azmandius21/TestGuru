637e027c3ac42d811dcde0b77adefe0c0ae63b85
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-09-19 15:49:42 +0500

    fix: cleaned application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,4 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(user)
     user.is_a?(Admin) ? admin_tests_path : root_path
   end
-
-  def is_you_are_admin
-    
-  end
 end
