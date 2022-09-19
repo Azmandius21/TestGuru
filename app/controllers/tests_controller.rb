818acde0e51c50f989c291792e8830d35a1a1890
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-09-19 17:54:57 +0500

    fix: action #start chnged 2

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -12,7 +12,7 @@ class TestsController < ApplicationController
   def start
     @test = Test.find(params[:id])
     current_user.tests.push(@test)
-    redirect_to @user.test_passage(@test)
+    redirect_to current_user.test_passage(@test)
   end
 
   private
