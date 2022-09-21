b400e6c8823edb27fd05901577cdddd28207d06c
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-09-22 02:00:08 +0500

    feat: for mail locales

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,10 +2,7 @@
 
 module QuestionsHelper
   def question_header(question)
-    if question.new_record?
-      "Create New Test #{question.test.title} Question"
-    else
-      "Edit Test #{question.test.title} Question"
-    end
+    key = question.new_record? ? 'create' : 'edit'
+    t("helpers.questions.#{key}", test: @question.test.title)
   end
 end
