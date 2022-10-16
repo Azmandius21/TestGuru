e712dde24e1bbda8953c011ad1603059c560fa85
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-10-16 11:45:07 +0500

    fix: gemfile and excepted GIT_TOKEN from code

--- Gemfile
+++ Gemfile
@@ -49,8 +49,6 @@ gem 'faraday-retry'
 
 gem 'octokit','~>5.0'
 
-gem 'octokit', '~> 5.0'
-
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -1,7 +1,6 @@
 class GitHubClient
 
   ROOT_ENDPOINT = 'https://api.github.com'
-  ACCESS_TOKEN = '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'
 
   def initialize
     @http_client = setup_http_client
@@ -9,7 +8,7 @@ class GitHubClient
 
   def create_gist(params)
     @http_client.post('gist') do |request|
-      request.headers['Authorization'] = "token #{ACCESS_TOKEN}"
+      request.headers['Authorization'] = "token #{ENV['GIT_TOKEN']}"
       request.headers['Content-Type'] = 'application/json'
       request.body = params.to_json
     end
