f91699583f7313237de2581c33056212f43c2985
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-10-20 00:22:36 +0500

    fix: table-striped table tests

--- app/assets/javascripts/utilities/sorting.js
+++ app/assets/javascripts/utilities/sorting.js
@@ -1,11 +1,11 @@
 document.addEventListener('turbolinks:load', function() {
   const control = document.querySelector('.sort-by-title')
 
-  control.addEventListener('click', sortRowsByTitle)
+  if (control) {control.addEventListener('click', sortRowsByTitle)}
 })
 
 function sortRowsByTitle() {
-  const table = document.querySelector('table')
+  let table = document.querySelector('table')
   const rows = document.querySelectorAll('tr')
 
   let sortedRows = []
@@ -15,4 +15,28 @@ function sortRowsByTitle() {
   }
 
   console.log(sortedRows)
+
+  sortedRows.sort(compareRows)
+
+  console.log(sortedRows)
+
+  const sortedTable = document.createElement('table')
+
+  sortedTable.classList.add('table','table-striped')
+  sortedTable.appendChild(rows[0])
+
+  for (let i = 0; i < sortedRows.length; i++ ) {
+    sortedTable.appendChild(sortedRows[i])
+  }
+
+  table.parentNode.replaceChild(sortedTable, table)
+}
+
+function compareRows(row1,row2) {
+  let testTitle1 = row1.querySelector('td').textContent
+  let testTitle2 = row2.querySelector('td').textContent
+
+  if (testTitle1 > testTitle2) { return 1}
+  if (testTitle1 < testTitle2) { return -1}
+  return 0
 }
