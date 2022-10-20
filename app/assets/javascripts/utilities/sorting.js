a03693549a7855a41ffe5bbe8ee385c072f24dec
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-10-20 08:44:49 +0500

    feat: sort-arrow by title tests#index for user and admin

--- app/assets/javascripts/utilities/sorting.js
+++ app/assets/javascripts/utilities/sorting.js
@@ -5,7 +5,7 @@ document.addEventListener('turbolinks:load', function() {
 })
 
 function sortRowsByTitle() {
-  let table = document.querySelector('table')
+  let table = document.querySelector('tbody')
   const rows = document.querySelectorAll('tr')
 
   let sortedRows = []
@@ -14,15 +14,19 @@ function sortRowsByTitle() {
     sortedRows.push(rows[i])
   }
 
-  console.log(sortedRows)
+  sortedRows.sort(compareRowsAsc)
+  if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
+   sortedRows.sort(compareRowsAsc)
+   this.querySelector('.octicon-arrow-up').classList.remove('hide')
+   this.querySelector('.octicon-arrow-down').classList.add('hide')
+  } else {
+   sortedRows.sort(compareRowsDesc)
+   this.querySelector('.octicon-arrow-up').classList.add('hide')
+   this.querySelector('.octicon-arrow-down').classList.remove('hide')
+  }
 
-  sortedRows.sort(compareRows)
+  const sortedTable = document.createElement('tbody')
 
-  console.log(sortedRows)
-
-  const sortedTable = document.createElement('table')
-
-  sortedTable.classList.add('table','table-striped')
   sortedTable.appendChild(rows[0])
 
   for (let i = 0; i < sortedRows.length; i++ ) {
@@ -32,11 +36,20 @@ function sortRowsByTitle() {
   table.parentNode.replaceChild(sortedTable, table)
 }
 
-function compareRows(row1,row2) {
+function compareRowsAsc(row1,row2) {
   let testTitle1 = row1.querySelector('td').textContent
   let testTitle2 = row2.querySelector('td').textContent
 
   if (testTitle1 > testTitle2) { return 1}
   if (testTitle1 < testTitle2) { return -1}
+  return 0
+}
+
+function compareRowsDesc(row1,row2) {
+  let testTitle1 = row1.querySelector('td').textContent
+  let testTitle2 = row2.querySelector('td').textContent
+
+  if (testTitle1 > testTitle2) { return -1}
+  if (testTitle1 < testTitle2) { return 1}
   return 0
 }
