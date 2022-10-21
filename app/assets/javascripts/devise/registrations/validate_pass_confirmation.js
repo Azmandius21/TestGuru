bd9d54eb481bb6c9e73d8da3b23190c88a24c722
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-10-21 13:44:31 +0500

    feat: js validate_pass_conf

---
+++ app/assets/javascripts/devise/registrations/validate_pass_confirmation.js
@@ -0,0 +1,11 @@
+document.addEventListener('turbolink:load', function() {
+  // const passwordConfirmation = document.querySelector('.password-confirmation')
+  // const password = document.querySelector('.password')
+
+  // passwordConfirmation.oninput = validatePassConfirmation(passwordConfirmation.value)
+})
+
+let passwordConfirmation = document.querySelector('#user_password_confirmation')
+passwordConfirmation.oninput = validatePassConfirmation {
+  consile.log(passwordConfirmation.value)
+}
