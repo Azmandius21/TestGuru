2e3bfd01f0bfbcf53e40a5192d1eade5f5a211d9
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-10-31 09:08:31 +0500

    feat: form_inline.js

---
+++ app/assets/javascripts/utilities/form_inline.js
@@ -0,0 +1,26 @@
+document.addEventListener('turbolinks:load', function() {
+  let controls = document.querySelectorAll('.form-inline-link')
+
+  if (controls.length) {
+    for (let i = 0; i < controls.length; i++) {
+      controls[i].addEventListener('click', formInlineLinkHandler)
+    }
+  }
+})
+
+function formInlineLinkHandler(event) {
+  event.preventDefault()
+  let testId = this.dataset.testId
+
+  formInlineHandler(testId)
+}
+
+function formInlineHandler(testId) {
+  let $link = $(.'form-inline-link[data-test-id="' + testId + '"]'')
+  let $testTitle = $(.'test-title[data-test-id="' + testId + '"]'')
+  let $formInline = $(.'form-inline[data-test-id="' + testId + '"]'')
+
+  if ($formInline.contains('hide')) {
+    $testTitle.add
+  }
+}
