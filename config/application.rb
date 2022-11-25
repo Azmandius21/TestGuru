856416f109bb7969691c6aac5abca1cf801ef9b4
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-11-25 21:51:43 +0500

    fix: remove: failures in the assets:precompile task

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module TestGuru
 
     config.autoload_paths << "#{Rails.root}/lib/clients"
 
-    config.assets.initialize_on_precompile = false
+    #config.assets.initialize_on_precompile = false
   end
 end
