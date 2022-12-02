086195a84e3e03972cff41dac7ba866960778574
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-12-02 21:37:50 +0500

    fix: add attributes for badge model

--- db/migrate/20221202133430_create_badges.rb
+++ db/migrate/20221202133430_create_badges.rb
@@ -1,8 +1,9 @@
 class CreateBadges < ActiveRecord::Migration[5.2]
   def change
     create_table :badges do |t|
-      t.text, :title
-      t.text, :picture_url
+      t.string :title, null: false
+      t.string :picture_url
+      t.string :reason_for_the_gift
 
       t.timestamps
     end
