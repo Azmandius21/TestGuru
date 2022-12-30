38fa67926c32dabffb4346af52728777da250602
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-12-30 21:32:45 +0500

    fix: rulemethods beginer & first_try

--- app/rule_methods/beginer_rule_method.rb
+++ app/rule_methods/beginer_rule_method.rb
@@ -1,7 +1,7 @@
 class BeginerRuleMethod < BaseRuleMethod
   def work
     if @user.badges.present?
-      unless @user.badges.find_by(name: "beginer")
+      unless @user.badges.find_by(title: "beginer")
         @success_test_passages.present?
       end
     end
