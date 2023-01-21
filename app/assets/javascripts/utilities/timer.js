80f234d913aa26e232bbb6ed7c31bf55413cd1a2
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2023-01-22 01:17:00 +0500

    fix: timer

--- app/assets/javascripts/utilities/timer.js
+++ app/assets/javascripts/utilities/timer.js
@@ -1,15 +1,31 @@
 document.addEventListener('turbolinks:load', function() {
   const beginTime = new Date().getTime()
+  const limitTime = 5*1000
   console.log(beginTime)
-  let interval = setInterval(countDownTime, 1000, beginTime)
-  clearInterval(interval)
+  
+  runInterval(beginTime, limitTime)
 })
+
+function runInterval(beginTime, limitTime) {
+  myInterval = setInterval(countDownTime, 1000, (beginTime + limitTime))
+ }
+  
+function stopTimer() {
+  clearInterval(myInterval);
+}
 
 function countDownTime(time) {
   let timeNow = new Date().getTime()
-  difTime = timeNow - time
-  // document.querySelector('#second').innerHtml = Math.floor(difTime/1000)
-  // document.querySelector('.minutes').innerHtml = Math.floor(difTime/(1000*60))
-  // document.querySelector('.hours').innerHTML = Math.floor(difTime/(1000*60*60))
-  console.log(difTime)
+  difTime = time - timeNow
+  
+  if (difTime >= 0) {
+    document.querySelector('.seconds').innerHTML = Math.floor(difTime/1000)
+    document.querySelector('.minutes').innerHTML = Math.floor(difTime/(1000*60))
+    document.querySelector('.hours').innerHTML = Math.floor(difTime/(1000*60*60))    
+  } else {
+    stopTimer()
+  }
+  console.log(difTime)  
 }
+
+
